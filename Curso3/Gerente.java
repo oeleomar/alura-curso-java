c969a104fb52983844f434ad370d37bfe74ee799
Eleomar Dorneles <eleomar.dorneles.18@outlook.com>
2023-01-29 17:34:33 -0400

    ok

--- Curso3/Gerente.java
+++ Curso3/Gerente.java
@@ -3,16 +3,21 @@ package Curso3;
 public class Gerente extends Funcionario {
   private int senha;
 
-  public Gerente (String nome, String cpf, double salario, int senha) {
+  public Gerente(String nome, String cpf, double salario, int senha) {
     super(nome, cpf, salario);
     this.senha = senha;
   }
 
   public boolean autentica(int senha) {
-    if(this.senha == senha) {
+    if (this.senha == senha) {
       return true;
-    }else {
+    } else {
       return false;
     }
+  }
+
+  @Override
+  public double getBonificacao() {
+    return super.getBonificacao() + super.getSalario();
   }
 }
--- Curso3/TesteFuncionario.java
+++ Curso3/TesteFuncionario.java
@@ -5,6 +5,7 @@ public class TesteFuncionario {
     Funcionario funcionario = new Funcionario("Eleomar Dorneles", "000.000.000-00", 2500);
     Gerente gerente = new Gerente("Eleomar Dorneles", "000.000.000-00", 2500, 12345678);
     System.out.println(gerente.autentica(12345678));
-    
+    System.out.println(gerente.getBonificacao());
+
   }
 }
