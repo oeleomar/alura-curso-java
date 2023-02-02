48780b54dd54faebc2a3ad58cf1d86b189e79a9c
Eleomar Dorneles <eleomar.dorneles.18@outlook.com>
2023-02-02 13:58:28 -0400

    ok

---
+++ java-alura-curso5/src/br/com/eleomardorneles/alura/test/MySystem.java
@@ -0,0 +1,17 @@
+package br.com.eleomardorneles.alura.test;
+
+public class MySystem {
+
+  /*
+   * Ententendo o System.out.println()
+   * System
+   * - Classe, java.lang, acesso público
+   * 
+   * out.
+   * - Atributo, publico, referencia, estático
+   * 
+   * println
+   * - Método, publico, não estático, sobrecarga, não joga exceções do tipo
+   * checked
+   */
+}
