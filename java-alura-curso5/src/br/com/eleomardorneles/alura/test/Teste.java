bc42fb0b56f34180d7860b9ea79f93ff92ce6651
Eleomar Dorneles <eleomar.dorneles.18@outlook.com>
2023-02-02 15:44:04 -0400

    ok

---
+++ java-alura-curso5/src/br/com/eleomardorneles/alura/test/Teste.java
@@ -0,0 +1,9 @@
+package br.com.eleomardorneles.alura.test;
+
+public class Teste {
+  public static void main(String[] args) {
+    System.out.println("x");
+    System.out.println(3);
+    System.out.println(false);
+  }
+}
