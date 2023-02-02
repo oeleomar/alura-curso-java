4721c9fedb2cc67310799e86412bb8aa263d010c
Eleomar Dorneles <eleomar.dorneles.18@outlook.com>
2023-02-01 21:47:24 -0400

    ok

---
+++ java-alura-curso5/src/br/com/eleomardorneles/alura/test/MinhaString.java
@@ -0,0 +1,18 @@
+package br.com.eleomardorneles.alura.test;
+
+public class MinhaString {
+  public static void main(String[] args) {
+    String nome = "Eleomar"; // Object literal
+    String outroNome = new String("Outro Nome");
+    // Char é sempre colocado com aspas simples ''
+    char a = 'a';
+
+    String novoNome = nome.replace('E', '.');
+    String nomeMinusculo = nome.toLowerCase();
+    String nomeMaiusculo = nome.toUpperCase();
+    String trim = nome.trim();
+    int indexOf = nome.indexOf('e');
+    char b = nome.charAt(1);
+    System.out.println(b);
+  }
+}
