eec853688843adbe737b5e871a88aae09f5f6d39
Eleomar Dorneles <eleomar.dorneles.18@outlook.com>
2023-02-05 11:26:44 -0400

    okj

--- java-alura-curso6/src/br/com/eleomardorneles/java/test/util/TesteClassesAnonimas.java
+++ java-alura-curso6/src/br/com/eleomardorneles/java/test/util/TesteClassesAnonimas.java
@@ -41,6 +41,12 @@ public class TesteClassesAnonimas {
     lista.add(cc4);
 
     // Classes anônimas
+    // Classes declaradas que executam apenas 1 método é chamadas de Function object
+    // Não é preciso criar todo um código, caso apenas eu queira uma classe que
+    // executa uma função.
+    // Por baixo dos panos o compilador irá criar uma classe, com uma nomeclatura
+    // definida por ele.
+    // Legibilidade bem complexa
     lista.sort(new Comparator<Conta>() {
       // Melhor método
       @Override
@@ -48,8 +54,7 @@ public class TesteClassesAnonimas {
         // Usando o wrapper Integer
         return Integer.compare(o1.getNumero(), o2.getNumero());
       }
-    }
-    );
+    });
     for (Conta conta : lista) {
       System.out.println(conta);
     }
