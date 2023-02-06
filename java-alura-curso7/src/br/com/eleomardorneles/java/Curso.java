6451eefe5a719593ec14fd53acc182d66f10b8b1
Eleomar Dorneles <eleomar.dorneles.18@outlook.com>
2023-02-06 09:15:58 -0400

    ok

--- java-alura-curso7/src/br/com/eleomardorneles/java/Curso.java
+++ java-alura-curso7/src/br/com/eleomardorneles/java/Curso.java
@@ -1,21 +1,31 @@
 package br.com.eleomardorneles.java;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 public class Curso {
   private String nome;
   private String instrutor;
   private List<Aula> aulas = new LinkedList<>();
+  private Set<Aluno> alunos = new HashSet<>();
 
   public Curso(String nome, String instrutor) {
+    if (nome == null) {
+      throw new NullPointerException("Nome não pode ser nulo");
+    }
     this.nome = nome;
     this.instrutor = instrutor;
   }
 
   public List<Aula> getAulas() {
     return Collections.unmodifiableList(this.aulas);
+  }
+
+  public Set<Aluno> getAlunos() {
+    return Collections.unmodifiableSet(this.alunos);
   }
 
   public String getNome() {
@@ -30,6 +40,10 @@ public class Curso {
     this.aulas.add(aula);
   }
 
+  public void matricular(Aluno aluno) {
+    this.alunos.add(aluno);
+  }
+
   public int getTempoTotal() {
     return this.aulas.stream().mapToInt(Aula::getTempo).sum();
   }
@@ -37,6 +51,10 @@ public class Curso {
   @Override
   public String toString() {
     return "[Curso: " + this.nome + ", tempo total " + this.getTempoTotal() + "]";
+  }
+
+  public boolean estaMatriculado(Aluno a1) {
+    return this.alunos.contains(a1);
   }
 
 }
